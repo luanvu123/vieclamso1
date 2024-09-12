878763fcfc3b1baa6a2bdabce02a995df0e89379
luanvu123 <phamvuluan131@gmail.com>
2024-09-12 12:09:52 +0700

    fg

--- resources/views/layouts/layout_login.blade.php
+++ resources/views/layouts/layout_login.blade.php
@@ -2,7 +2,7 @@
 <html lang="zxx">
 
 <head>
-    <title>Logdy - Login and Register Form HTML5 Template</title>
+    <title>Vieclamso1 - Tuyendungso1 Login and Register </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
     <link type="text/css" rel="stylesheet" href="{{ asset('backend-template/assets/css/bootstrap.min.css') }}">
